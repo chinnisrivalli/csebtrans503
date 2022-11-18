b9f94286a5ed0bca50c19e91bc6df2e9daaccdfe
91830 <91830@srivalli>
2022-11-18 21:59:56 +0530

    this is my first sonar qube

---
+++ src/main/java/Csebtrans503/Csebtrans503/App.java
@@ -0,0 +1,13 @@
+package Csebtrans503.Csebtrans503;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Csebtrans503/Csebtrans503/AppTest.java
@@ -0,0 +1,20 @@
+package Csebtrans503.Csebtrans503;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
